48e43b0061ffd388d3ccf490b85dd9151287b647
Tetsuya Mori <monry84@gmail.com>
2017-08-03 15:58:58 +0900

    :seedling: Initial commit

---
+++ Assets/Scripts/AcecssorUtility/AnimatorAccessor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace AccessorUtility {
+
+    /// <summary>
+    /// DI っぽく MonoBehaviour に Animator() メソッドを提供するためのインタフェース
+    /// </summary>
+    public interface IAnimatorAccessor {
+    }
+
+    /// <summary>
+    /// IAnimatorAccessor の拡張クラス
+    /// </summary>
+    public static class AnimatorAccessorExtension {
+
+        /// <summary>
+        /// IAnimatorAccessor を実装しているクラスに `.Animator()` として Getter を提供する
+        /// </summary>
+        /// <param name="self">IAnimatorAccessor のインスタンス</param>
+        /// <returns>Animator のインスタンス</returns>
+        public static Animator Animator(this IAnimatorAccessor self) {
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            MonoBehaviour monoBehaviour = self as MonoBehaviour;
+            if (monoBehaviour == null) {
+                return null;
+            }
+            // 未セットの場合に、GetComponent した結果をキャッシュさせる
+            if (!monoBehaviour.PropertyExists<Animator>()) {
+                Animator animator = monoBehaviour.gameObject.GetComponent<Animator>();
+                monoBehaviour.PropertySet(animator);
+            }
+            return monoBehaviour.PropertyGet<Animator>();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,38 @@
+# What?
+
+* `this.Animator()` で参照出来る擬似的なアクセサを提供します
+
+# Why?
+
+* 毎回アクセサを書くのがシンドイので
+
+# Install
+
+```shell
+$ npm install @umm/animator_accessor
+```
+
+# Usage
+
+```csharp
+using UnityEngine;
+using AccessorUtility;
+
+public class Hoge : MonoBehaviour, IAnimatorAccessor {
+
+    public void Start() {
+        this.Animator().SetTrigger("Fuga");
+    }
+
+}
+```
+
+1. AccessorUtility.IAnimatorAccessor を実装します
+1. 拡張メソッドとして Animator() が提供されます
+
+# License
+
+Copyright (c) 2017 Tetsuya Mori
+
+Released under the MIT license, see [LICENSE.txt](LICENSE.txt)
+
